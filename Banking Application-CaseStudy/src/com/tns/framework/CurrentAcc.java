b49f08e856949f9e4b9a096ce0f6258518813120
Madhujya Dutta <madhujyadutta007@gmail.com>
2022-04-11 12:35:31 +0530

    CASE STUDY

---
+++ Banking Application-CaseStudy/src/com/tns/framework/CurrentAcc.java
@@ -0,0 +1,42 @@
+package com.tns.framework;
+
+
+public abstract class CurrentAcc extends BankAcc {
+	private float creditLimit;
+
+	public float getCreditLimit() {
+		return creditLimit;
+	}
+
+//	Constructor
+	public CurrentAcc(int accNo, String accNm, float accBal, float creditLimit) {
+		super(accNo, accNm, accBal); //superclass parent objects
+		this.creditLimit = creditLimit; //this is used to implify current objects
+	}
+
+	@Override
+	public void withdraw(float amt) {
+		if (amt > getAccBal() + creditLimit) {
+			System.out.println("insufficient balance".toUpperCase());
+			System.out.println("Trasaction Failed.");
+		} else if (getAccBal() >= amt) {
+			setAccBal(getAccBal() - amt);
+			System.out.println("Trasaction Complete.");
+			
+		} else {
+			amt -= getAccBal();
+			creditLimit -= amt;
+			setAccBal(0);
+			System.out.println("Trasaction Complete.");
+		}
+	}
+
+	@Override
+	public String toString() {
+		System.out.println("Your account Number is " + (getAccNo()));
+		System.out.println("Your Account Balance is " + getAccBal() + " & Your Credit Limit is " + creditLimit);
+
+		return "CurrentAcc []";
+	}
+
+}
